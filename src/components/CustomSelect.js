a20c462c5767b2626d360ce7dab31481c4f4de5f
CodeSevenic <sevenicbookings@gmail.com>
2022-09-25 07:20:15 +0200

    Topco Module Code Update

--- src/components/CustomSelect.js
+++ src/components/CustomSelect.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 
-const CustomSelect = () => {
+const CustomSelect = ({
+  label,
+  data,
+  value,
+  name,
+  onChange,
+  error,
+  defaultOptionLabel,
+}) => {
+  const [selectedValue, setSelectedValue] = useState(value);
+  const [selectedIndex, setSelectedIndex] = useState(
+    value !== '' ? data.indexOf(value) : null,
+  );
+  const [search, setSearch] = useState('');
+  const [options, setOptions] = useState(data);
   return <div></div>;
 };
 
