91797a7a13ec0fbf983b98145820b364eba89790
CodeSevenic <sevenicbookings@gmail.com>
2022-09-25 15:05:37 +0200

    Topco Module Code update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
+import CustomSelect from './components/CustomSelect';
+import { countries } from './data';
 
 function App({ moduleData }) {
   // eslint-disable-next-line no-console
@@ -7,6 +9,61 @@ function App({ moduleData }) {
     'all of your data typically accessed via the "module" keyword in HubL is available as JSON here!',
     moduleData,
   );
+
+  const [formData, setFormData] = useState({
+    countryOne: {
+      value: '',
+      error: '',
+    },
+
+    countryTwo: {
+      value: 'Croatia',
+      error: '',
+    },
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    let errors = {};
+
+    for (let key in formData) {
+      if (formData[key].value === '') {
+        errors[key] = 'Please select one option';
+      }
+    }
+
+    if (Object.keys(errors).length === 0) {
+      console.log(formData.countryOne, formData.countryTwo.value);
+      console.log('submit form...');
+    } else {
+      setFormData((prev) => {
+        let data = {};
+        for (let key in errors) {
+          data[key] = {
+            ...prev[key],
+            error: errors[key],
+          };
+        }
+
+        return {
+          ...prev,
+          ...data,
+        };
+      });
+    }
+  };
+
+  const changeHandler = (value, name) => {
+    setFormData((prev) => ({
+      ...prev,
+      [name]: {
+        value,
+        error: value !== '' ? '' : prev[name].error,
+      },
+    }));
+  };
+
   return (
     <section className="topco-d-container">
       <div className="topco-d-wrapper">
@@ -25,6 +82,18 @@ function App({ moduleData }) {
             <option value="opel">Opel</option>
             <option value="audi">Audi</option>
           </select>
+        </div>
+        <div className="topco-brand-sectors">
+          <form action="" onSubmit={submitHandler}>
+            <CustomSelect
+              label={'Select a country'}
+              searchPlaceholder="Search"
+              data={countries}
+              value={formData.countryOne.value}
+              onChange={changeHandler}
+              error={formData.countryOne.error}
+            />
+          </form>
         </div>
       </div>
     </section>
