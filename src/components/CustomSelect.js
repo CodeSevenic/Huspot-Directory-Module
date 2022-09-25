6dab9ef439a8c3d48ad1de483b5d9adb4df2383f
CodeSevenic <sevenicbookings@gmail.com>
2022-09-25 09:53:29 +0200

    Topco Module Code update

--- src/components/CustomSelect.js
+++ src/components/CustomSelect.js
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 
+import Dropdown from './Dropdown';
+
 const CustomSelect = ({
   label,
   data,
@@ -15,7 +17,79 @@ const CustomSelect = ({
   );
   const [search, setSearch] = useState('');
   const [options, setOptions] = useState(data);
-  return <div></div>;
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownEl = useRef();
+
+  // Hide dropdown if clicked outside of dropdown
+  const handleClickOutside = useCallback(
+    (e) => {
+      if (showDropdown && e.target.closet('.dropdown') !== dropdownEl.current) {
+        setShowDropdown(false);
+        setSearch('');
+        setOptions(data);
+      }
+    },
+    [showDropdown, setShowDropdown, dropdownEl, data],
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
+  const changeSelectedHandler = (item, name, index) => {
+    setSelectedValue(item);
+    setSelectedIndex(index);
+    setShowDropdown(false);
+    onChange(item, name);
+  };
+
+  const searchChangeHandler = (e) => {
+    setSearch(e.target.value);
+    const filteredOptions = data.filter((opt) => {
+      return opt.toLowerCase().include(e.target.value.trim().toLowerCase());
+    });
+
+    setOptions(filteredOptions);
+  };
+
+  return (
+    <div className="form__group">
+      <label htmlFor="">{label}</label>
+      <div className="dropdown" ref={dropdownEl}>
+        <input
+          type="hidden"
+          name={name}
+          value={value}
+          onChange={() => onChange(value, name)}
+        />
+        <div
+          className="dropdown__selected"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          {selectedValue
+            ? selectedValue
+            : defaultOptionLabel
+            ? defaultOptionLabel
+            : 'Please select one option'}
+        </div>
+        {showDropdown && (
+          <Dropdown
+            search={search}
+            searchChangeHandler={searchChangeHandler}
+            options={options}
+            selectedValue={selectedValue}
+            selectedIndex={selectedIndex}
+            changeSelectedHandler={changeSelectedHandler}
+          />
+        )}
+      </div>
+      {error && <div className="error">{error}</div>}
+    </div>
+  );
 };
 
 export default CustomSelect;
---
+++ src/components/Dropdown.js
@@ -0,0 +1,55 @@
+﻿import React, { useEffect, useRef } from 'react';
+const Dropdown = ({
+  searchPlaceholder,
+  search,
+  searchChangeHandler,
+  options,
+  selectedValue,
+  changeSelectedHandler,
+  name,
+  selectedIndex,
+}) => {
+  const searchInputEl = useRef();
+  const itemsEl = useRef();
+
+  useEffect(() => {
+    searchInputEl.current.focus();
+    if (selectedValue) {
+      itemsEl.current.scrollTop =
+        itemsEl.current.querySelector(`.item-${selectedIndex}`).offsetTop - 42;
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="dropdown__menu">
+      <input
+        type="text"
+        placeholder={searchPlaceholder ? searchPlaceholder : 'Search...'}
+        className="dropdown__menu_search"
+        value={search}
+        onChange={searchChangeHandler}
+        ref={searchInputEl}
+      />
+      <div className="dropdown__menu_items" ref={itemsEl}>
+        {options.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                selectedValue === item
+                  ? `dropdown__menu_item item-${index} selected`
+                  : `dropdown__menu_item item-${index}`
+              }
+              onClick={() => changeSelectedHandler(item, name, index)}
+            >
+              {item}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
